325b2fefc7068c1792c40c4ef2fceb1768eab72f
myOmikron <accounts@omikron.dev>
2020-09-26 04:53:39 +0200

    Added try to catch decode errors

--- main.py
+++ main.py
@@ -13,46 +13,48 @@ import socketserver
 
 import serial
 
-
 STORAGE = {}
 
 Packet = collections.namedtuple("Packet", ["rssi", "timestamp", "port"])
 
-error = functools.partial(print, file = sys.stderr)
+error = functools.partial(print, file=sys.stderr)
 
 
 def add_packet(line: bytes, args: argparse.Namespace, name: str) -> typing.Optional[Packet]:
     ts = time.monotonic()
-    mac, rssi, crc = line.rstrip().decode("ASCII").split(" ")
-
-    while len(crc) < 8:
-        crc = "0" + crc
-
     try:
-        rssi = int(rssi)
-    except ValueError as exc:
-        error(f"ValueError: {exc}")
-    if len(mac) != 17:
-        error(f"Dropping weird packet from MAC {mac}!")
+        mac, rssi, crc = line.rstrip().decode("ascii").split(" ")
 
-    if len(args.filters) > 0:
-        if mac not in args.filters:
-            return
+        while len(crc) < 8:
+            crc = "0" + crc
 
-    packet = Packet(rssi = rssi, timestamp = ts, port = name)
-    if args.verbose:
-        print(f"{mac}: {rssi} ({crc}) @ {ts:.6f}")
+        try:
+            rssi = int(rssi)
+        except ValueError as exc:
+            error(f"ValueError: {exc}")
+        if len(mac) != 17:
+            error(f"Dropping weird packet from MAC {mac}!")
 
-    if mac not in STORAGE:
-        STORAGE[mac] = {}
-    if crc not in STORAGE[mac]:
-        STORAGE[mac][crc] = []
-    STORAGE[mac][crc].append(packet)
+        if len(args.filters) > 0:
+            if mac not in args.filters:
+                return
 
-    if len(STORAGE[mac][crc]) == len(args.ports):
-        calculate(STORAGE[mac][crc])
-        del STORAGE[mac][crc]
+        packet = Packet(rssi=rssi, timestamp=ts, port=name)
+        if args.verbose:
+            print(f"{mac}: {rssi} ({crc}) @ {ts:.6f}")
 
+        if mac not in STORAGE:
+            STORAGE[mac] = {}
+        if crc not in STORAGE[mac]:
+            STORAGE[mac][crc] = []
+        STORAGE[mac][crc].append(packet)
+
+        if len(STORAGE[mac][crc]) == len(args.ports):
+            calculate(STORAGE[mac][crc])
+            del STORAGE[mac][crc]
+
+    except:
+        pass
 
 class TCPCollectionHandler(socketserver.StreamRequestHandler):
     """
@@ -70,7 +72,7 @@ class TCPCollectionHandler(socketserver.StreamRequestHandler):
         try:
             add_packet(line, TCPCollectionHandler.args, str(self.server.server_address))
         except ValueError:
-            traceback.print_exc(file = sys.stderr)
+            traceback.print_exc(file=sys.stderr)
 
     def handle(self) -> None:
         while True:
@@ -97,7 +99,7 @@ def calculate(packets: list) -> None:
 
 
 def start(func, path: str, args: argparse.Namespace) -> None:
-    t = threading.Thread(target= func, args = (path, args), daemon = True)
+    t = threading.Thread(target=func, args=(path, args), daemon=True)
     t.start()
     if args.verbose:
         print(f"Thread {t} has been started on serial device {path}")
@@ -105,61 +107,61 @@ def start(func, path: str, args: argparse.Namespace) -> None:
 
 def setup() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        description = "WiFi Tracking PoC"
+        description="WiFi Tracking PoC"
     )
 
     parser.add_argument(
         "-v", "--verbose",
-        help = "print verbose information",
-        dest = "verbose",
-        action = "store_true"
+        help="print verbose information",
+        dest="verbose",
+        action="store_true"
     )
 
     parser.add_argument(
         "-b", "--baud",
-        help = "Baud rate of the connected serial devices (default 115200)",
-        dest = "baud",
-        type = int,
-        default = 115200
+        help="Baud rate of the connected serial devices (default 115200)",
+        dest="baud",
+        type=int,
+        default=115200
     )
 
     parser.add_argument(
         "-f", "--filter",
-        help = "specify MAC addresses to filter against (white-list)",
-        dest = "filters",
-        action = "append",
-        default = []
+        help="specify MAC addresses to filter against (white-list)",
+        dest="filters",
+        action="append",
+        default=[]
     )
 
     parser.add_argument(
         "-p", "--port",
-        help = "specify the serial port(s) to listen to, may be used multiple times",
-        dest = "ports",
-        action = "append",
-        default = []
+        help="specify the serial port(s) to listen to, may be used multiple times",
+        dest="ports",
+        action="append",
+        default=[]
     )
 
     parser.add_argument(
         "-n", "--network",
-        help = "specify the TCP port(s) to listen too, may be used multiple times",
-        dest = "network",
-        action = "append",
-        type = int,
-        default = []
+        help="specify the TCP port(s) to listen too, may be used multiple times",
+        dest="network",
+        action="append",
+        type=int,
+        default=[]
     )
 
     parser.add_argument(
         "-c", "--counter",
-        help = "max. number of collected packets, use 0 to disable (default 1024)",
-        dest = "counter",
-        type = int,
-        default = 1024
+        help="max. number of collected packets, use 0 to disable (default 1024)",
+        dest="counter",
+        type=int,
+        default=1024
     )
 
     parser.add_argument(
         "-j", "--json",
-        help = "path to the JSON file to store the resulting data",
-        dest = "json"
+        help="path to the JSON file to store the resulting data",
+        dest="json"
     )
 
     return parser
@@ -175,7 +177,7 @@ if __name__ == "__main__":
         start(collect, device, arguments)
     for port in arguments.network:
         s = socketserver.ThreadingTCPServer(("0.0.0.0", port), TCPCollectionHandler)
-        process = threading.Thread(target = s.serve_forever, daemon = True)
+        process = threading.Thread(target=s.serve_forever, daemon=True)
         process.start()
         if arguments.verbose:
             print(f"Server thread {process} has been started on {port}")
@@ -188,6 +190,6 @@ if __name__ == "__main__":
 
     if arguments.json is not None:
         with open(arguments.json, "w") as f:
-            json.dump(STORAGE, f, indent = 4)
+            json.dump(STORAGE, f, indent=4)
 
     print("Main thread has exited!")
